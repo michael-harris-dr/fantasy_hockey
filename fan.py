e72df826b46d985c9f4246b8df2fbf242d740aff
Michael Harris <harriswork110@gmail.com>
2023-01-19 12:27:56 -0500

    toying with NHLs own API

--- fan.py
+++ fan.py
@@ -6,6 +6,14 @@ import requests
 import json
 from datetime import date
 
+
+class Player:
+    def __init__(self, name, goals, assists, shooting_percentage):
+        self.name = name
+        self.goals = goals
+        self.assists = assists
+        self.shooting_percentage = shooting_percentage
+
 keyStr = "33667pxcyxpqsjsx7d65bf22"
 
 apiStr = "http://api.sportradar.us/nhl/trial/v7/en/seasons/2022/REG/leaders/offense.json?api_key=" + keyStr
@@ -30,4 +38,9 @@ json.dump(statsJson, fp, skipkeys=False, ensure_ascii=True, check_circular=True,
 #5	faceoffs_won
 #6+	even str./shorthanded/per-game stats
 
-print(statsJson["categories"][1]["ranks"][0]["rank"])
+apiStr2 = "https://statsapi.web.nhl.com/api/v1/people/8478483/stats?stats=statsSingleSeason&season=20222023"
+response = requests.get(apiStr2)
+print(response.text)
+
+for i in range(5):
+    print(statsJson["categories"][1]["ranks"][i]["player"]["full_name"] + " " + str(statsJson["categories"][1]["ranks"][i]["statistics"]["total"]["goals"]))
