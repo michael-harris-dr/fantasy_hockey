bf31020a9bbad4ad3f9411e7b3d39e6ab9d96da4
Michael Harris <harriswork110@gmail.com>
2023-02-04 13:39:52 -0500

    functionization improved

--- fan.py
+++ fan.py
@@ -22,11 +22,6 @@ def update_team_list():
     print("Teams list finished update.")
 
 def update_player_list(teamID):
-    # 
-    #
-    # Find list of players on each team
-    #
-    #
     apiStr = f"https://statsapi.web.nhl.com/api/v1/teams/{teamID}/roster" #?expand=team.roster"
     response = requests.get(apiStr)
     print(f"Updating player list for team id={teamID}, response: {response.status_code}")
@@ -38,7 +33,7 @@ def update_player_list(teamID):
         teamJson["copyright"] = teamID #changes copyright text to team ID
         #print(nhlJson)
     
-        fp = open(f"TEAM_ROSTER_{teamID}.json", "w")
+        fp = open(f"db/TEAM_ROSTER_{teamID}.json", "w")
         json.dump(teamJson, fp, skipkeys=False, ensure_ascii=True, check_circular=True, allow_nan=True, cls=None, indent=3, separators=None)
 
         fp.close()
@@ -48,7 +43,7 @@ def update_entire_player_list(idList):
         update_player_list(id)
     print("All rosters updated...")
 
-def inputNames():
+def input_names():
     #print("Enter player names in the form \"lastname,lastname,lastname\":")
     inp = input("Enter player surnames:")
     nameList = inp.replace(" ",",").split(',')
@@ -72,78 +67,154 @@ def menu_loop(idTeam):
 def fanPts(player):
     return (3*player["goals"] + 2*player["assists"])
 
-idTeam = {}#dict()
+def find_players(nameList, idTeam):
+    #find players in the league and store their IDs
+    playerIDs = {}
 
-fp = open("NHL_TEAMS.json", "r")
-nhlJson = json.load(fp)
-for team in nhlJson["teams"]: #for each team
-    idTeam[team["id"]] = team["name"] #add the team name to the dict w/ the team ID as the key
+    for player in nameList: #for every player the user requested
+        for id in idTeam:   #for every id in the team id list (for each NHL team)
+            tmp = player
+            #open and load the corresponding JSON for the current team
+            fp2 = open(f"./db/TEAM_ROSTER_{id}.json")
+            teamJson = json.load(fp2)
 
-menu_loop(idTeam)
-
-nameList = inputNames()
-
-playerIDs = {}
-playerStats = {}
-
-#find players in the league and store their IDs
-for player in nameList: #for every player the user requested
-    for id in idTeam:   #for every id in the team id list (for each NHL team)
-        tmp = player
-        #open and load the corresponding JSON for the current team
-        fp2 = open(f"TEAM_ROSTER_{id}.json")
-        teamJson = json.load(fp2)
-
-        for name in teamJson["roster"]: #for every player on the team
-            surname = name["person"]["fullName"].split(" ")[1]  #isolate their last name
-            
-            if(player == surname):  #if the last name of the current player matches the last name of the current desired player
-                print(f"Found {player} matches {surname} on team: " + idTeam[teamJson["copyright"]])
+            for name in teamJson["roster"]: #for every player on the team
+                surname = name["person"]["fullName"].split(" ")[1]  #isolate their last name
                 
-                #accounting for repeated names (e.g. "Tkachuk")
-                while player in playerIDs:
-                    player = f"{player}*"
-                    #print(playerIDs)
-                playerIDs[player] = name["person"]["id"]    #add the current player as a key in the dict with their value as their ID
-            player = tmp
-        fp2.close()
-print(playerIDs)
+                if(player == surname):  #if the last name of the current player matches the last name of the current desired player
+                    print(f"Found {player} matches {surname} on team: " + idTeam[teamJson["copyright"]])
+                    
+                    #accounting for repeated names (e.g. "Tkachuk")
+                    while player in playerIDs:
+                        player = f"{player}*"
+                        #print(playerIDs)
+                    playerIDs[player] = name["person"]["id"]    #add the current player as a key in the dict with their value as their ID
+                player = tmp
+            fp2.close()
+    return playerIDs
 
 
-refinedPlayerStats = {}
+def populate_stats(playerIDs, yearStrings):
+    playerStats = {}
 
-goals = 0
-assists = 0
-shotPct = 0.0
-games = 0
+    goals = 0
+    assists = 0
+    shotPct = 0
+    games = 0
+
+    for years in yearStrings:
+        for player in playerIDs:
+            apiStr = f"https://statsapi.web.nhl.com/api/v1/people/{playerIDs[player]}/stats?stats=statsSingleSeason&season={years}"
+            response = requests.get(apiStr)
+            playerJson = json.loads(response.text)
+            print(response.status_code)
+            #print(playerJson)
+            #print(playerJson["stats"][0]["splits"][0]["stat"])
+            if "shutouts" in playerJson["stats"][0]["splits"][0]["stat"]:
+                print(f"{player} is goalie")
+            else:
+                stats = playerJson["stats"][0]["splits"][0]["stat"]
+            
+                goals += stats["goals"]
+                assists += stats["assists"]
+                shotPct += stats["shotPct"]
+                games += stats["games"]
+            playerStats[player] = {"goals":goals, "assists":assists, "shotPct":shotPct, "games":games}
+    return playerStats
 
 #TODO: add players previous years stats and adjust xFP (expected fantasy points) accordingly
 #TODO: add GP as stat and factor into xFP
-#adding a player's stats to the playerStats dict
-for player in playerIDs:
-    apiStr = f"https://statsapi.web.nhl.com/api/v1/people/{playerIDs[player]}/stats?stats=statsSingleSeason&season=20222023"
-    response = requests.get(apiStr)
-    playerJson = json.loads(response.text)
-    print(response.status_code)
-    print(playerJson)
-    #print(playerJson["stats"][0]["splits"][0]["stat"])
-    if "shutouts" in playerJson["stats"][0]["splits"][0]["stat"]:
-        print(f"{player} is goalie")
-    else:
-        stats = playerJson["stats"][0]["splits"][0]["stat"]
-       
-        goals = stats["goals"]
-        assists = stats["assists"]
-        shotPct = stats["shotPct"]
-        games = stats["games"]
+def populate_player_stats(playerIDs):
+    #adding a player's stats to the playerStats dict
+    playerStats = {}
 
-        refinedPlayerStats[player] = {"goals":goals, "assists":assists, "shotPct":shotPct, "games":games}
+    for player in playerIDs:
+        apiStr = f"https://statsapi.web.nhl.com/api/v1/people/{playerIDs[player]}/stats?stats=statsSingleSeason&season=20222023"
+        response = requests.get(apiStr)
+        playerJson = json.loads(response.text)
+        print(response.status_code)
+        #print(playerJson)
+        #print(playerJson["stats"][0]["splits"][0]["stat"])
+        if "shutouts" in playerJson["stats"][0]["splits"][0]["stat"]:
+            print(f"{player} is goalie")
+        else:
+            stats = playerJson["stats"][0]["splits"][0]["stat"]
+        
+            goals = stats["goals"]
+            assists = stats["assists"]
+            shotPct = stats["shotPct"]
+            games = stats["games"]
 
-print(refinedPlayerStats)
+            playerStats[player] = {"goals":goals, "assists":assists, "shotPct":shotPct, "games":games}
+    return playerStats
 
-#isolating useful stats
-for player in refinedPlayerStats:
-    print(f"fanPts for {player} = {fanPts(refinedPlayerStats[player])}")
+def populate_past_stats(playerIDs):
+    #adding a player's stats to the playerStats dict
+    pastStats = {}
 
-fp.close()
+    for player in playerIDs:
+        apiStr = f"https://statsapi.web.nhl.com/api/v1/people/{playerIDs[player]}/stats?stats=statsSingleSeason&season=20212022"        
+        response = requests.get(apiStr)
+        playerJson = json.loads(response.text)
+        
+        print(response.status_code)
+        #print(playerJson)
+        #print(playerJson["stats"][0]["splits"][0]["stat"])
+        if "shutouts" in playerJson["stats"][0]["splits"][0]["stat"]:
+            print(f"{player} is goalie")
+        else:
+            stats = playerJson["stats"][0]["splits"][0]["stat"]
+        
+            goals = stats["goals"]
+            assists = stats["assists"]
+            shotPct = stats["shotPct"]
+            games = stats["games"]
+
+            pastStats[player] = {"goals":goals, "assists":assists, "shotPct":shotPct, "games":games}
+
+            apiStr = f"https://statsapi.web.nhl.com/api/v1/people/{playerIDs[player]}/stats?stats=statsSingleSeason&season=20202021"        
+            response = requests.get(apiStr)
+            playerJson = json.loads(response.text)
+
+            stats = playerJson["stats"][0]["splits"][0]["stat"]
+
+            pastStats[player]["goals"] += stats["goals"]
+            pastStats[player]["assists"] += stats["assists"]
+            pastStats[player]["games"] += stats["games"]
+
+            print(((pastStats[player]["shotPct"] * pastStats[player]["games"]) + (stats["shotPct"] * stats["games"])) / (pastStats[player]["games"] + stats["games"]))
+
+    return pastStats
+
+#MAIN()###########################################################################################################
+
+def main():
+    idTeam = {}#dict()
+
+    fp = open("NHL_TEAMS.json", "r")
+    nhlJson = json.load(fp)
+    for team in nhlJson["teams"]: #for each team
+        idTeam[team["id"]] = team["name"] #add the team name to the dict w/ the team ID as the key
+
+    menu_loop(idTeam)
+
+    nameList = input_names()
+
+    playerIDs = find_players(nameList, idTeam)
+    #print(playerIDs)
+
+    yearIDs = ["20222023"]
+    playerStats = populate__stats(playerIDs, yearIDs)
+    print(playerStats)
+
+    pastStats = populate_past_stats(playerIDs)
+    print(pastStats)
+
+    #isolating useful stats
+    for player in playerStats:
+        print(f"fanPts for {player} = {fanPts(playerStats[player])}")
+
+    fp.close()
+
+main()
 #FIN
