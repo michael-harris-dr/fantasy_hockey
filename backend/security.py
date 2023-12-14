28000a586c0f6701fcc8a8e477e9bbbf59c0afa7
Michael Harris <harriswork110@gmail.com>
2023-12-13 22:28:43 -0500

    manual keys for testing

--- backend/security.py
+++ backend/security.py
@@ -8,7 +8,7 @@ from helpy import myself
 API_KEY_HEADER = APIKeyHeader(name="X-API-key")
 
 def validate_api_key(x_api_key: str = Security(API_KEY_HEADER)):
-	keyList = os.environ.get('KEYS')
+	keyList = ["temp120681689", "4132"]#os.environ.get('KEYS')
 	print("KEYLIST, GET YER KEYLIST HERE:")
 	print(keyList)
 	if x_api_key in keyList:
